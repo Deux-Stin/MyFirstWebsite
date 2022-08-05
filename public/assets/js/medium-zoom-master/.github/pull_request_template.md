4a4f1ab216f41711e5862b49d46ca52ea817fcd1
Dustyn Naya <dustyn.naya@renault.com>
2022-08-05 16:17:20 +0200

    Delivery on main branch
    Signed-off-by: Dustyn Naya <dustyn.naya@renault.com>

---
+++ public/assets/js/medium-zoom-master/.github/pull_request_template.md
@@ -0,0 +1,12 @@
+<!--
+  Thank you for submitting a pull request 🙌
+  This template helps you create an effective feature report.
+-->
+
+## Summary
+
+<!-- Explain why you are making this change and link related issues (#ISSUE_NUMBER) -->
+
+## Result
+
+<!-- Explain what you've changed and what's the result! -->
---
+++ public/assets/js/reusable_js.js
@@ -0,0 +1,73 @@
+//Reusable javascripts
+
+//Rolling text initial work from https://codepen.io/gabrielcojea/pen/GRpZbZL -->
+
+(function($) {
+
+let elements = document.querySelectorAll(".rolling-text");
+
+elements.forEach((element) => {
+  let innerText = element.innerText;
+  element.innerHTML = "";
+
+  let textContainer = document.createElement("div");
+  textContainer.classList.add("block");
+
+  for (let letter of innerText) {
+    let span = document.createElement("span");
+    span.innerText = letter.trim() === "" ? "\xa0" : letter;
+    span.classList.add("letter");
+    textContainer.appendChild(span);
+  }
+
+  element.appendChild(textContainer);
+  element.appendChild(textContainer.cloneNode(true));
+});
+
+
+})(jQuery);
+
+
+// Fonction permettant de mettre en forme la barre de couleur sous les titres d'articles :
+var widthToGet = document.getElementsByClassName("widthToGet");
+var widthTarget = document.getElementsByClassName("widthTarget");
+for (var i = 0; i < widthToGet.length; i++) {
+    resizeElements(widthToGet.item(i), widthTarget.item(i));
+}
+
+
+function resizeElements(toGet, target) {
+    var widthToGet = toGet.clientWidth;
+    let widthTarget = target;
+
+    const text = toGet.textContent;
+    const font = "16px Poppins, sans-serif"
+    const canvas = document.createElement("canvas");
+    const context = canvas.getContext("2d");
+    context.font = font;
+
+    const {
+        width
+    } = context.measureText(text);
+
+    widthTarget.style.width = width + 'px';
+    // console.log(width+'px');
+}
+
+
+// GSAP use for articles expand
+const myElement = document.querySelectorAll(".blog-card");
+
+var hover = false;
+
+myElement.forEach(function (element, i) {
+  gsap.set(element, { width: "max-content", height: "5.5vh" });
+
+  element.addEventListener("mouseover", () => {
+    gsap.to(element, { width: "max-content", height: "max-content"});
+  });
+
+  element.addEventListener("mouseleave", () => {
+    gsap.to(element, { width: "max-content", height: "5.5vh"});
+  });
+});
